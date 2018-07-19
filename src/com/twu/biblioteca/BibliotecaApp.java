2a530d424ef7e91f841f520795f29ad7a71b0226
mmbarnett <michael@thirdstar.org>
2018-07-19 14:23:09 -0500

    1 MS/MB Welcome message

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,8 +1,19 @@
 package com.twu.biblioteca;
 
+import java.io.*;
+
 public class BibliotecaApp {
+    private PrintStream printStream;
+
+    public BibliotecaApp(PrintStream ps) {
+        printStream = ps;
+    }
 
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+    }
+
+    public void printGreeting() {
+        printStream.println("Welcome to Biblioteca!");
     }
 }
