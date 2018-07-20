18ab1353a9a6f484a3d377535c6b3709ae426894
sbriley <susiebriley@gmail.com>
2018-07-20 15:35:04 -0500

    3 SR/MB Can perform list books from input command

--- src/com/twu/biblioteca/Menu.java
+++ src/com/twu/biblioteca/Menu.java
@@ -1,20 +1,38 @@
 package com.twu.biblioteca;
 
+import java.io.IOException;
+
 public class Menu {
-    public enum MenuCodes {
+
+
+    public enum MenuCode {
         BookList,
-        Checkout
+        Checkout,
+        Invalid
     }
 
 
     private final Library lib;
+    private final UserInterface ui;
 
-    public Menu(Library lib) {
+    public Menu(Library lib, UserInterface ui) {
 
         this.lib = lib;
+        this.ui = ui;
     }
 
-    public String executeInput(MenuCodes i) {
+    public MenuCode parseIntToMenuCode (int i) {
+        switch (i) {
+            case 1:
+                return MenuCode.BookList;
+            case 2:
+                return MenuCode.Checkout;
+
+        }
+        return MenuCode.Invalid;
+    }
+
+    public String executeInput(MenuCode i) {
 
 
         switch (i) {
@@ -24,6 +42,17 @@ public class Menu {
                 return "Thank you! Enjoy the book";
         }
         return null;
+    }
+
+    public void doRequest() throws IOException {
+        String userInput = ui.getUserInput();
+
+        // TODO: assume for now the userInput is fine
+
+        MenuCode menuCode = parseIntToMenuCode(Integer.parseInt(userInput));
+        String response = executeInput(menuCode);
+
+        ui.printMessage(response);
     }
 
 }
--- test/com/twu/biblioteca/MenuTest.java
+++ test/com/twu/biblioteca/MenuTest.java
@@ -1,31 +1,56 @@
 package com.twu.biblioteca;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintStream;
+
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class MenuTest {
+
+    private UserInterface ui;
+    private Library lib;
+    private Menu menu;
+    private PrintStream printStream;
+    private BufferedReader input;
+
+    @Before
+    public void setup() {
+        printStream = mock(PrintStream.class);
+        input = mock(BufferedReader.class);
+
+        ui = new UserInterface(printStream, input);
+        lib = new Library(LibraryTest.testBookList);
+
+        menu = new Menu(lib, ui);
+    }
 
     @Test
     public void shouldGetBookListWhenGivenInput1() {
 
-        Library lib = new Library(LibraryTest.testBookList);
-        Menu menu = new Menu(lib);
-
-        assertEquals(lib.getBookListString(), menu.executeInput(Menu.MenuCodes.BookList));
-
-    }
-    @Test
-    public void shouldShowIfOptionIsInvalid()
-    {
+        assertEquals(lib.getBookListString(), menu.executeInput(Menu.MenuCode.BookList));
 
     }
 
     @Test
     public void shouldReturnIfBookWasCheckedOutSuccessfully()
     {
-        Library lib = new Library(LibraryTest.testBookList);
-        Menu menu = new Menu(lib);
-        assertEquals("Thank you! Enjoy the book", menu.executeInput(Menu.MenuCodes.Checkout));
+        assertEquals("Thank you! Enjoy the book", menu.executeInput(Menu.MenuCode.Checkout));
+    }
+
+    @Test
+    public void shouldMakeUIPrintListOfBooksIf1IsTyped() throws IOException {
+
+        when(input.readLine()).thenReturn("1");
+
+        menu.doRequest();
+
+        verify(printStream).println(lib.getBookListString());
     }
 }
