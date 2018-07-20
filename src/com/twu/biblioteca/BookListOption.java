43ecad8b1da26924bf6fad49737d9b7641fdde9a
Megan Sullivan <megansullivan@minint-r9hb80h.corporate.thoughtworks.com>
2018-07-20 16:18:34 -0500

    NJ/AT modified execute method in the Booklist Option class and shouldPrintBookList in the BookListOptionTest class

--- src/com/twu/biblioteca/BookListOption.java
+++ src/com/twu/biblioteca/BookListOption.java
@@ -1,4 +1,23 @@
 package com.twu.biblioteca;
 
-public class BookListOption {
+public class BookListOption extends Option {
+
+
+    private Library lib;
+    private UserInterface ui;
+
+    public BookListOption(Library lib, UserInterface ui) {
+
+        this.lib = lib;
+        this.ui = ui;
+    }
+
+    @Override
+    public boolean execute() { //May need more clarification on purpose of this method.
+        BookListOption option = new BookListOption(lib, ui);
+        option.lib.getBookListString();
+
+
+        return true;
+    }
 }
--- src/com/twu/biblioteca/Option.java
+++ src/com/twu/biblioteca/Option.java
@@ -3,4 +3,6 @@ package com.twu.biblioteca;
 public abstract class Option {
 
     public abstract boolean execute();
+
+
 }
